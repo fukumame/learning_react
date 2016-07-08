03a144b8065ce775cd27154993dbf72d6d3a341f
Atsushi Fukuda <atsushi-fukuda@bm-sms.co.jp>
2016-07-09 07:04:45 +0900

    チュートリアルに従って実装

---
+++ public/scripts/example3.js
@@ -0,0 +1,86 @@
+var Comment = React.createClass({
+
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    var md = new Remarkable();
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(
+      function(comment) {
+        return (
+          <Comment author={comment.author} key={comment.id}>
+            {comment.text}
+          </Comment>
+        );
+      }
+    )
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      // thisオブジェクトに対して、successの無名関数を紐付けた関数を返す
+      // http://qiita.com/hosomichi/items/e11ad0c4ea79db2dee84
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    })
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval)
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <CommentList data={this.state.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox url="/api/comments" pollInterval="2000" />,
+  document.getElementById('content')
+);
